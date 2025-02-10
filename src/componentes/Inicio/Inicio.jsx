e6336f696b1a8e96c90f647b8085f0d07768321b
ul1sessacuna <ulisesacuna90@gmail.com>
2025-02-10 11:25:25 -0300

    nuevo

--- src/componentes/Inicio/Inicio.jsx
+++ src/componentes/Inicio/Inicio.jsx
@@ -13,8 +13,10 @@ const Inicio = () => {
     const locutores = [
         { dia: "lunes", hora: 10, nombre: "Oscar Almiron", programa: "Lo Mas Escuchado", foto: "./oscar.jpg" },
         { dia: "martes", hora: 10, nombre: "Oscar Almiron", programa: "Lo Mas Escuchado", foto: "./oscar.jpg" },
+        { dia: "martes", hora: 18, nombre: "Gilda Prieto", programa: "Tiempo Real", foto: "./gildapietro.jpg" },
         { dia: "miércoles", hora: 10, nombre: "Oscar Almiron", programa: "Lo Mas Escuchado", foto: "./oscar.jpg" },
         { dia: "jueves", hora: 10, nombre: "Oscar Almiron", programa: "Lo Mas Escuchado", foto: "./oscar.jpg" },
+        { dia: "jueves", hora: 18, nombre: "Gilda Prieto", programa: "Tiempo Real", foto: "./gildapietro.jpg" },
         { dia: "viernes", hora: 10, nombre: "Oscar Almiron", programa: "Lo Mas Escuchado", foto: "./oscar.jpg" },
         { dia: "lunes", hora: 12, nombre: "Sin Locutor", programa: "Programacion Habitual", foto: "./fm.jpg" },
         { dia: "martes", hora: 12, nombre: "Sin Locutor", programa: "Programacion Habitual", foto: "./fm.jpg" },
